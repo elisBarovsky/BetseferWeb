6a79442e9fd82f52d261501d906f771996d9e74f
Ellis Barovsky <33927309+elisBarovsky@users.noreply.github.com>
2018-05-11 11:59:16 +0300

    gitush

--- Admin_Add_Messages_JS.js
+++ Admin_Add_Messages_JS.js
@@ -1,6 +1,9 @@
-﻿
+﻿alert(5);
+
+
+alert('zayine');
+
 $('#messageTypeRBL').on('change', function () {
     alert($('input[name=radioName]:checked', '#messageTypeRBL').val());
 });
 
-alert(zayine);
