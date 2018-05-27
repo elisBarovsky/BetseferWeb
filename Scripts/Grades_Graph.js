986c10bfb1eaf696306c3c7b1fc6bb392273c069
Ellis Barovsky <33927309+elisBarovsky@users.noreply.github.com>
2018-05-27 18:09:18 +0300

    סקריעפטים עודכנו

---
+++ Scripts/Grades_Graph.js
@@ -0,0 +1,108 @@
+﻿
+$(document).ready(onDeviceReady);
+
+function onDeviceReady() {
+
+ localStorage.setItem("LastVisit", "Grades.html"); //saving in localS
+ //   Grade.ID = localStorage.getItem("UserID");
+
+    var queryString = decodeURIComponent(window.location.search);
+    var queryString2 = decodeURIComponent(window.location.search);
+    queryString = queryString.substring(4,14);
+    queryString2 = queryString2.substring(21);
+    localStorage.setItem("PupilGrade", queryString2);
+    GradeDate = new Object();
+    GradeDate.Date = queryString;
+    GivenGradeByCode(GradeDate, renderGivenGradeByDate);
+
+}
+
+
+
+function renderGivenGradeByDate(results) {
+    //this is the callBackFunc 
+    results = $.parseJSON(results.d);
+    var counter = 0;
+    var counter1 = 0;
+    var GradeAvg = 0;
+    var PupilGradeThis = localStorage.getItem("PupilGrade");
+    var GradePos = 0;
+    for (var i = 0; i < results.length; i++) {
+        if (results[counter1].Grade === PupilGradeThis) {
+            GradePos = i + 1;
+        }
+        GradeAvg += results[counter1].Grade;
+        counter1++;
+    }
+    GradeAvg = (GradeAvg / results.length);
+
+    var PupilGrades = [];
+    var PupilGradesAVG = [];
+    var GradeThisPupil = [];
+    GradeThisPupil.push({ x: GradePos, y: parseInt(PupilGradeThis) });
+
+    for (i = 0; i < results.length; i++) {
+        PupilGrades.push({ x: i + 1, y: results[counter++].Grade });
+        PupilGradesAVG.push({ x: i + 1, y: GradeAvg });
+    }
+    PupilGrades = PupilGrades.sort();
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title: {
+            text: "בחינה ב" + results[0].LessonName + ". המורה: " + results[0].TeacherName 
+        },
+        axisX: {
+            valueFormatString: "#"
+        },
+        axisY: {
+            maximum: 120,
+            title: "ציונים",
+            includeZero: true
+        },
+        legend: {
+            cursor: "pointer",
+            fontSize: 14,
+            itemclick: toggleDataSeries
+        },
+        toolTip: {
+            shared: true
+        },
+        data: [{
+            markerColor: "blue",
+            markerType: "cross",
+            markerSize: 15,
+            type: "line",
+            showInLegend: true,
+            legendText: "הציון שלי",
+            dataPoints: GradeThisPupil
+        },
+        {
+            type: "area",
+            legendText: "ממוצע כיתתי",
+            showInLegend: true,
+            fillOpacity: .3,
+            lineThickness: 7,
+            dataPoints: PupilGradesAVG
+        },
+        {
+            type: "spline",
+            legendText: "ציוני הכיתה",
+            showInLegend: true,
+            dataPoints: PupilGrades
+        }]
+    });
+    chart.render();
+
+    function toggleDataSeries(e) {
+        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        }
+        else {
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+ 
+
+}
---
+++ Scripts/Notes.js
@@ -0,0 +1,80 @@
+﻿$(document).ready(onDeviceReady);
+
+UserInfoNote = new Object();
+
+function onDeviceReady() {
+
+    localStorage.setItem("LastVisit", "Notes.html"); //saving in localS
+    UserInfoNote.ID = localStorage.getItem("PupilID");
+    $(function () {
+        $("#accordion").accordion({
+            collapsible: true
+        });
+    });
+    GetUserNotes(UserInfoNote, renderNotes);
+}
+
+function renderNotes(results) {
+    res = $.parseJSON(results.d);
+    var counter = 0;
+
+    $("#accordion").accordion();
+
+    var ImgIcon;
+    for (var i = 0; i < res.length; i++) {
+        if (res[counter].NoteName === "הצטיינות") {
+            ImgIcon = "Images/happy.png";
+        }
+        else {
+            ImgIcon = "Images/sad.png";
+        }
+
+        var newIMG = document.createElement("img");
+        newIMG.setAttribute('src', ImgIcon);
+        newIMG.setAttribute('height', '55px');
+        newIMG.setAttribute('style', 'padding-right:38%');
+
+        var newH3 = document.createElement('h3');
+
+        var newDiv = document.createElement('div');
+
+        var newP0 = document.createElement('p');
+        var newP1 = document.createElement('p');
+        var newP2 = document.createElement('p');
+        var newP3 = document.createElement('p');
+        var acc = document.getElementById('accordion');
+        var div = document.getElementById("Div1");
+
+        newH3.innerText = 'סוג הערה: ' + res[counter].NoteName + '                         ';
+
+        newH3.appendChild(newIMG);
+
+        newP0.innerText = 'מורה: ' + res[counter].Teacher_FullName;
+        newDiv.appendChild(newP0);
+
+        newP1.innerText = 'מקצוע: ' + res[counter].LessonName ;
+        newDiv.appendChild(newP1);
+
+        newP2.innerText = 'תאריך: ' + res[counter].NoteDate;
+        newDiv.appendChild(newP2);
+
+        newP3.innerText = 'תיאור הערה: ' + res[counter].Comment;
+        newDiv.appendChild(newP3);
+
+        //get inside the accordion!
+        acc.appendChild(newH3);
+        acc.appendChild(newDiv);
+        $("#accordion").accordion("refresh");
+
+       // $('#accordion').append('<h3><img src="' + ImgIcon + '"style="height: 100px; float: left"/>סוג הערה: ' + res[counter].NoteName + '</h3><div><p>מקצוע: ' + res[counter].LessonName + '<br />תאריך: ' + res[counter].NoteDate + ' </p></div>');
+        //$("#accordion").accordion();
+     //   $('#accordion').append('<h3>First Floor</h3><div><p>Welcome to da first floor.</p></div> <h3>Floor numbah 2</h3><div><p>you are now on floor 2</p></div>');
+      //  $("#accordion").accordion()
+       // dynamicLy = "<img src='" + ImgIcon + "'style='height:100px;float:left'/> <h3>סוג הערה: " + res[counter].NoteName + "</h3><div><p>מקצוע: " + res[counter].LessonName + "</p><p>תאריך: " + res[counter].NoteDate + "</p></div>";
+      //  dynamicLy = "<li> <a href='#' data-id=" + res[counter].CodeGivenNote + "><img src='" + ImgIcon + "' /> <p>סוג הערה: " + res[counter].NoteName + "</p><p>מקצוע: " + res[counter].LessonName + "</p><p>תאריך: " + res[counter].NoteDate + "</p> </li>";
+        counter++;
+    }
+
+  //  $('#accordion').listview('refresh');
+
+}
