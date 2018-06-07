432223f29b736085fb03677116a0458c164e8236
Ellis Barovsky <33927309+elisBarovsky@users.noreply.github.com>
2018-06-07 23:21:33 +0300

    השוואה

--- App_Code/DBconnectionTeacher.cs
+++ App_Code/DBconnectionTeacher.cs
@@ -84,10 +84,10 @@ public class DBconnectionTeacher
 
     public DataTable PupilGrades(string PupilID)  //New !! 
     {
-        string selectSTR = " SELECT dbo.Grades.GradeCode, dbo.Lessons.LessonName, dbo.Grades.ExamDate, dbo.Grades.Grade,dbo.Grades.PupilID " +
+        string selectSTR = " SELECT dbo.Grades.GradeCode, dbo.Lessons.LessonName, dbo.Grades.ExamDate, dbo.Grades.Grade,dbo.Grades.PupilID , (select [UserFName]+' '+[UserLName]  from [dbo].[Users] where [UserID]= dbo.Grades.TeacherID) as Teacher_FullName" +
                         " FROM  dbo.Grades INNER JOIN dbo.Users ON dbo.Grades.PupilID = dbo.Users.UserID  INNER JOIN " +
                         "  dbo.Pupil ON dbo.Users.UserID = dbo.Pupil.UserID INNER JOIN  dbo.Lessons ON dbo.Grades.CodeLesson = dbo.Lessons.CodeLesson " +
-                        " where dbo.Grades.PupilID = '" + PupilID + "'";
+                        " where dbo.Grades.PupilID = '"+ PupilID + "' order by dbo.Grades.ExamDate desc";
         DataTable dtt = new DataTable();
         DataSet ds;
         try
