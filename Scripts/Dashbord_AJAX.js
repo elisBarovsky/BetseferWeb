36673de73e15b17be52513c737cb351eb72dc00a
Dandik100 <33927352+Dandik100@users.noreply.github.com>
2018-06-07 12:40:57 +0300

    hfdghfdh

--- Scripts/Dashbord_AJAX.js
+++ Scripts/Dashbord_AJAX.js
@@ -1,1 +1,31 @@
-﻿
+﻿function LoadAllMessagesById(Id, DisplayMessages) {
+    $.ajax({
+        url: 'BetseferWS.asmx/GetMessagesByUserId',
+        data: JSON.stringify({ 'userId': Id }),
+        type: 'POST',
+        dataType: "json",
+        contentType: 'application/json; charset = utf-8',
+        success: function (results) {
+            DisplayMessages(results);
+        },
+        error: function (request, error) {
+            alert('Network error has occurred please try again!');
+        }
+    });
+}
+
+function UpdateMessageAsRead(i) {
+    $.ajax({
+        url: 'BetseferWS.asmx/SetMessageAsRead',
+        data: JSON.stringify({ 'MessageCode': i }),
+        type: 'POST',
+        dataType: "json",
+        contentType: 'application/json; charset = utf-8',
+        success: function (results) {
+
+        },
+        error: function (request, error) {
+            alert('Network error has occurred please try again!');
+        }
+    });
+}
--- Scripts/Dashbord_JS.js
+++ Scripts/Dashbord_JS.js
@@ -3,6 +3,38 @@
     var z = localStorage.getItem("UserImg");
     document.getElementById('imgUser').src = z;
     document.getElementById('UserImgimg').src = z;
-    
-    //LoadAllMessagesById(teacherId, DisplayMessages);
+    LoadAllMessagesById(Id, DisplayMessages);
 });
+
+function DisplayMessages(results) {
+
+    res = $.parseJSON(results.d);
+
+    $('#messagesTable').empty();
+
+    var tableString = "";
+
+    for (var i = 0; i < res.length; i++) {
+        var objMessage = new Object();
+        objMessage.MessageCode = res[i].MessageCode;
+        objMessage.MessageDate = res[i].MessageDate;
+        objMessage.SenderID = res[i].SenderID;
+        objMessage.SenderName = res[i].SenderName;
+        objMessage.SubjectMessage = res[i].SubjectMessage;
+        objMessage.TheMessage = res[i].TheMessage;
+
+        tableString += "<tr onclick = 'OpenMessage(" + JSON.stringify(objMessage) + ")'><td id = '" + res[i].MessageCode + "'></td><td class='mailbox-star'><a href='#'><i class='fa fa-star text-yellow'></i></a></td><td>" +
+            res[i].MessageDate + "</td><td>" + res[i].SubjectMessage + "</td><td>" + res[i].SenderName + "</td></tr>";
+    }
+    $('#messagesTable').append(tableString);
+};
+
+
+function OpenMessage(obj) {
+    localStorage.setItem("messageDetails", JSON.stringify(obj));
+    var i = obj.MessageCode;
+    UpdateMessageAsRead(i);
+    var a = window.open("OpenMessageWindow.html", "", "toolbar=no,scrollbars=yes,resizable=yes,top=50%,left=25%,width=500,height=600");
+    //window.open.href = "OpenMessageWindow.html";
+    //  window.location.href = "OpenMessageWindow.html";
+};
